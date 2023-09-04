7b51b947cc28b39ec9699f8f97c0a9f0a7519eae
MokhatlaM <119861055+MokhatlaM@users.noreply.github.com>
2023-09-04 10:24:37 +0200

    enumerators

---
+++ Five-Card Daw/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FiveCardDraw
+{
+    /*Enums:
+    Hand, CardValue, CardSuit
+
+    Methods:
+    Card - get, Set
+    Get Deck
+    Shuffle Deck
+    Deal Cards
+    Check Cards/Hand Strength*/
+    enum Value
+    {
+        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+    enum CardSuit
+    {
+        Diamonds,
+        Hearts,
+        Spades,
+        Clubs
+    }
+
+  enum PlayerHand
+    {
+        NoPair, OnePair, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush
+    }
+
+    class Card
+    {
+        public CardSuit Suit { get; set; }
+        public Value Value { get; set; }
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+        }
+       
+
+    }
+}
